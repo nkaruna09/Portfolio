4cf033a137d12d83de5e84a03d091b98d6b703e1
Nithika <nithika.karuna@gmail.com>
2024-12-23 23:19:44 -0500

    finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import Hero from './sections/hero/hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
+import Navbar from './sections/Navbar/Navbar'; 
 
 function App() {
   return ( 
     <div className="App">
+      <Navbar /> 
       <Hero />
       <Skills />
       <Projects /> 
---
+++ src/sections/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './NavbarStyles.module.css'; 
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navLinks}>
+        <li><a href="#hero">Home</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
